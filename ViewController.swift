5c4a3d1c330bf9a18603b35c39cb2e669659a78b
Love Hulterström <love.hulterstrom@benify.com>
2022-09-06 15:01:53 +0200

    Working popup x2

---
+++ ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  PopupSPA
+//
+//  Created by Love Hulterström on 2022-09-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var popupOutlet: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func popupAction(_ sender: Any) {
+        let alert = UIAlertController(title: "POP IT!", message: "You have been served. Click 'Boom' to self destruct", preferredStyle: .alert)
+        
+        let userAgreement = UIAlertAction(title: "Read user agreement", style: .default) { (action) in print(action)
+        }
+        
+        let privacyPolicy = UIAlertAction(title: "Read privacy policy", style: .default) { (action) in print(action)
+        }
+        let okayAction = UIAlertAction(title: "Okay", style: .default) { (action) in print(action)
+        }
+        
+        let disagreeAction = UIAlertAction(title: "Disagree", style: .destructive) { (action) in self.popup2()
+        }
+        
+        alert.addAction(userAgreement)
+        alert.addAction(privacyPolicy)
+        alert.addAction(disagreeAction)
+        alert.addAction(okayAction)
+        
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func popup2() {
+        let alert = UIAlertController(title: "POP IT2!", message: "You really wanna disagree? Come on, it'll be fun!", preferredStyle: .alert)
+        
+        let okayAction = UIAlertAction(title: "Okay", style: .default) { (action) in print(action)
+        }
+        
+        let disagreeAction = UIAlertAction(title: "Disagree", style: .destructive) { (action) in print(action)
+        }
+        
+        alert.addAction(okayAction)
+        alert.addAction(disagreeAction)
+        
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+}
+
